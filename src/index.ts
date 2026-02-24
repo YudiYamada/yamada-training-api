87df6eade9580f0a7d400a8d7589470f6048259a
Yudi Yamada <yudiyamada10@gmail.com>
2026-02-24 15:07:55 -0300

    feat: add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello, world!");
+import "dotenv/config";
+
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", function (request, reply) {
+  reply.send({ hello: "world" });
+});
+
+fastify.listen(
+  { port: Number(process.env.PORT) || 8081 },
+  function (err, address) {
+    if (err) {
+      fastify.log.error(err);
+      process.exit(1);
+    }
+  },
+);
